a4f9bae33415c85486ef689fa03be8d5f26e1800
Vladislav Prusakov <spectraldragon@bk.ru>
2025-02-17 13:13:11 +0300

    Initial Commit

---
+++ Brackeys2025/Brackeys2025App.swift
@@ -0,0 +1,17 @@
+//
+//  Brackeys2025App.swift
+//  Brackeys2025
+//
+//  Created by vladislav.prusakov on 17.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct Brackeys2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brackeys2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Brackeys2025
+//
+//  Created by vladislav.prusakov on 17.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
